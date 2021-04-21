f8e1a79adb3069434ad8fcb53309a76c32021574
xcf-t <pascal.kosak@gmail.com>
2021-04-21 20:34:10 +0200

    Implement motd endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { pathExists } from 'fs-extra';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
@@ -7,5 +9,27 @@ export class AppController {
     async signature(@Res() res: Response) {
         res.contentType('text/plain');
         res.send('omori.cloud');
+    }
+
+    @Get('motd')
+    async motd(@Res() res: Response) {
+        const motdFile = join(process.cwd(), 'motd.txt');
+        const exists = await pathExists(motdFile);
+
+        if (exists) {
+            res.sendFile(motdFile);
+        } else {
+            res.status(204).send();
+        }
+    }
+
+    @Get('generate_204')
+    async generate204(@Res() res: Response) {
+        res.status(402).send();
+    }
+
+    @Get('generate_402')
+    async generate402(@Res() res: Response) {
+        res.status(204).send();
     }
 }
